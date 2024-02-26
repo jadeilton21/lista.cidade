2bbb0816460925ee74525eb2582e7223b7226d84
Jadeilton <jadeiltondm@gmail.com>
2024-02-26 14:49:39 -0300

    feat: commit home

---
+++ src/test/java/lista/cidades/lista/cidade/ApplicationTests.java
@@ -0,0 +1,13 @@
+package lista.cidades.lista.cidade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
