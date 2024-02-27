372c1af90d8c93b621957e2e772d1286d0c862f7
Jadeilton <jadeiltondm@gmail.com>
2024-02-27 13:48:40 -0300

    feat: create arguments in class service

--- src/main/java/lista/cidades/lista/cidade/service/CidadeService.java
+++ src/main/java/lista/cidades/lista/cidade/service/CidadeService.java
@@ -1,4 +1,27 @@
 package lista.cidades.lista.cidade.service;
 
+import lista.cidades.lista.cidade.fein.CidadeCliente;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class CidadeService {
+
+
+
+    private final CidadeCliente cidadeCliente;
+
+
+    public CidadeService(CidadeCliente cidadeCliente){
+        this.cidadeCliente = cidadeCliente;
+    }
+
+
+
+    public List<Cidade> getCidadeFromLibriary(){
+        CidadeListResponse response = this.cidadeCliente.getCidade();
+
+        return response.getCidadeList();
+    }
 }
