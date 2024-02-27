77349fd19c2c23317ad1e54eba7539ad33f4f0e0
Jadeilton <jadeiltondm@gmail.com>
2024-02-27 14:08:24 -0300

    feat: create Cidade

--- src/main/java/lista/cidades/lista/cidade/response/CidadeListResponse.java
+++ src/main/java/lista/cidades/lista/cidade/response/CidadeListResponse.java
@@ -1,4 +1,20 @@
 package lista.cidades.lista.cidade.response;
 
+import lista.cidades.lista.cidade.doMain.Cidade;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class CidadeListResponse {
+
+
+   private List<Cidade> cidadeList;
+
+
+
 }
