db77f91bbe0a47a4d2111a9f2ddea49d31d5d332
Jadeilton <jadeiltondm@gmail.com>
2024-02-27 14:11:57 -0300

    feat: create Cidade

--- src/main/java/lista/cidades/lista/cidade/doMain/Cidade.java
+++ src/main/java/lista/cidades/lista/cidade/doMain/Cidade.java
@@ -1,4 +1,17 @@
 package lista.cidades.lista.cidade.doMain;
 
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
 public class Cidade {
+
+
+    private String nome;
+    private Integer habitantes;
+    private Integer turistas;
+    private String isbn;
+    private String imagens;
 }
