cac738172bf0ac75693e828b30b2deb0d5382779
Jadeilton <jadeiltondm@gmail.com>
2024-02-27 13:30:46 -0300

    feat: create CidadeController.java

--- src/main/java/lista/cidades/lista/cidade/controllers/CidadeController.java
+++ src/main/java/lista/cidades/lista/cidade/controllers/CidadeController.java
@@ -1,5 +1,19 @@
 package lista.cidades.lista.cidade.controllers;
 
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/cidade")
 public class CidadeController {
+
+
+
+
+
+
+
+
+
 }
