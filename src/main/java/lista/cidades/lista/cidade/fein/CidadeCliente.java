f14010c1b883bdac304b4c7d1ac15929e8e434e4
Jadeilton <jadeiltondm@gmail.com>
2024-02-27 14:05:38 -0300

    feat: create CidadeCliente

--- src/main/java/lista/cidades/lista/cidade/fein/CidadeCliente.java
+++ src/main/java/lista/cidades/lista/cidade/fein/CidadeCliente.java
@@ -1,4 +1,14 @@
 package lista.cidades.lista.cidade.fein;
 
+import lista.cidades.lista.cidade.response.CidadeListResponse;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@FeignClient
 public interface CidadeCliente {
+
+    @GetMapping
+    CidadeListResponse getCidade();
+
+
 }
