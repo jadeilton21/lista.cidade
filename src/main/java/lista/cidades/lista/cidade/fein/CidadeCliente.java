791f3246695b836dac532713dc91823e6cfcbb2a
Jadeilton <jadeiltondm@gmail.com>
2024-02-27 14:43:16 -0300

    feat: create Cidade

--- src/main/java/lista/cidades/lista/cidade/fein/CidadeCliente.java
+++ src/main/java/lista/cidades/lista/cidade/fein/CidadeCliente.java
@@ -4,7 +4,7 @@ import lista.cidades.lista.cidade.response.CidadeListResponse;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@FeignClient
+@FeignClient(name = "api-cidade", url = "https://cidade-exemplo-api.s3.us-east-2.amazonaws.com/lista.Json")
 public interface CidadeCliente {
 
     @GetMapping
